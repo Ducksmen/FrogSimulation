ff9b538c7f0953592b7ed3d9cd8c0d0a09c368bd
BT_1N3_08 <jhoang6606@bths.edu>
2018-11-19 10:15:43 -0500

    a

--- src/FrogSimulation.java
+++ src/FrogSimulation.java
@@ -11,26 +11,44 @@ public class FrogSimulation
 
     private int hopDistance()
     {
-        return (int)(Math.random()* 61) + -30;
+        return (int)(Math.random()* 21) + -5;
     }
 
     public boolean simulate()
     {
         int curD = 0;
         int hopD = 0;
+        String hops = "";
+        int[] arr = new int[maxHops];
         for(int i = 0;i < maxHops;i++)
         {
             hopD = hopDistance();
             curD += hopD;
+            arr[i] = hopD;
             if(curD<0)
             {
+                for(int j=0;j<i+1;j++)
+                {
+                    hops += arr[j] + " ";
+                }
+                System.out.println("Goal : " + goalDistance + " Hops: " + hops + " Failed Reached Negative Distance");
                 return false;
             }
             if(curD>=goalDistance)
             {
+                for(int j=0;j<i+1;j++)
+                {
+                    hops += arr[j] + " ";
+                }
+                System.out.println("Goal : " + goalDistance + " Hops: " + hops + " Success Final Distance: " + curD);
                 return true;
             }
         }
+        for(int j=0;j<maxHops;j++)
+        {
+            hops += arr[j] + " ";
+        }
+        System.out.println("Goal : " + goalDistance + " Hops: " + hops + " Failed Final Distance: " + curD);
         return false;
     }
 
